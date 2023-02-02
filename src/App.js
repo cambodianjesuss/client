a82a929f9e9160ec33aee5427d5c2547d4989955
Boi Soth <cambodianjesuss@gmail.com>
2023-02-01 23:58:10 -0800

    had to remove comment in json file haha

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/home-page/index.jsx';
+import HomePage from 'scenes/home-page';
+import LoginPage from 'scenes/login-page';
+import ProfilePage from 'scenes/profile-page';
 
-
-import './index.css';
+import 'index.css';
 
 function App() {
   return <div className="app">Mah</div>
